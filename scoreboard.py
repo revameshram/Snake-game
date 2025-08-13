bd646e5320aadb45f47c4a5289926d72a2a3929e
revameshram <revameshram28@gmail.com>
2025-08-13 12:34:14 +0530

    new highscore file

--- scoreboard.py
+++ scoreboard.py
@@ -1,9 +1,16 @@
 from turtle import Turtle
+import os
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        with open("highscore_data.py",mode="r") as file:
+        
+        
+        if not os.path.exists("highscore_data.txt"):
+            with open("highscore_data.txt", "w") as file:
+                file.write("0")
+        
+        with open("highscore_data.txt",mode="r") as file:
              self.high_score=int(file.read())
         self.score=0
         self.color("white")
@@ -15,7 +22,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score  
-            with open("highscore_data.py",mode="w") as file:
+            with open("highscore_data.txt",mode="w") as file:
              file.write(str(self.high_score))
 
         self.score=0
@@ -28,10 +35,4 @@ class Scoreboard(Turtle):
     def increase(self):
         self.score+=1
         self.update_high_score()
-        
-    # def gameover(self):
-    #     self.goto(0,0)
-    #     self.write(f"Gameover.",False,"center",('Arial',14,'normal'))
-        
-
-        
+    
